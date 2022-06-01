eb9c1889a623ef95c1648f23fd83d376bdee6d83
Brandon Kunkel <brandon@pieces.app>
2022-06-01 12:34:39 -0400

    Added optional searchPath to the loadDynamicLibrary() funciton

--- lib/src/loader.dart
+++ lib/src/loader.dart
@@ -28,27 +28,41 @@ String fullLibraryName(String name) => libraryPrefix() + name + systemLibExtensi
 
 /// Load the dynamic library and throw more verbose exceptions to improve debugging
 /// in cases where dynamic libraries exist but lack necessary dependencies
-Future<DynamicLibrary> loadDynamicLibrary(String name) async {
-  String libraryFile = fullLibraryName(name);
-  bool fileExists = await File(name).exists();
+Future<DynamicLibrary> loadDynamicLibrary({required String libraryName, String? searchPath}) async {
+  late String libraryPath;
 
-  if (!fileExists) {
-    throw Exception('loadDynamicLibrary Error: $libraryFile cannot be found\n'
-        'Current Directory: ${p.current}'
-        'Full Path Checked: ${p.absolute(libraryFile)}');
-  }
+  // Get the platform specific file name
+  String libraryFile = fullLibraryName(libraryName);
 
-  try {
+  // Build the dynamic library path from a search path or just a file name
+  if (searchPath != null && searchPath.isNotEmpty) {
+    // Early Exit if the desired search directory doesn't exist
+    Directory directory = Directory(searchPath);
+    if (!directory.existsSync()) {
+      throw Exception('loadDynamicLibrary Error: Search Path directory does not exist\n'
+          'Directory: ${directory.path}');
+    }
+    libraryPath = p.join(searchPath, libraryFile);
+  } else {
     // Handle weird edge case where running Dart on Linux needs
     // The './' prepended to the dynamic library file name
-    if (Platform.resolvedExecutable.endsWith('dart') && Platform.isLinux) {
-      return DynamicLibrary.open('./$libraryFile');
-    } else {
-      return DynamicLibrary.open(libraryFile);
-    }
+    libraryPath = Platform.resolvedExecutable.endsWith('dart') && Platform.isLinux ? './$libraryFile' : libraryFile;
+  }
+
+  // Check to see that the Dynamic Library file exists before trying to load it
+  if (!File(libraryPath).existsSync()) {
+    throw Exception('loadDynamicLibrary Error: $libraryName cannot be found\n'
+        'Current Directory: ${p.current} \n'
+        'Desired Path: $libraryPath}\n'
+        'Resolved Full Path: ${p.absolute(libraryFile)}');
+  }
+
+  // Try loading the dynamic library
+  try {
+    return DynamicLibrary.open(libraryPath);
   } catch (e) {
     ProcessResult dependencyCheckResult = await callOSDependencyCheck(libraryFile);
-    throw Exception('loadDynamicLibrary Error: $libraryFile cannot be loaded, it may be missing some dependencies.\n'
+    throw Exception('loadDynamicLibrary Error: $libraryFile cannot be loaded. It may not have all of its dependencies\n'
         'Dependency Check Results [stderr]: ${dependencyCheckResult.stderr}\n'
         'Dependency Check Results [stdout]: ${dependencyCheckResult.stdout}\n');
   }
@@ -59,7 +73,7 @@ Future<DynamicLibrary> loadDynamicLibrary(String name) async {
 /// - Windows: `dumpbin /DEPENDENTS [LIBRARY_PATH]`
 /// - MacOS: `otool -L [LIBRARY_PATH]`
 /// - Linux: `ldd [LIBRARY_PATH]`
-Future<ProcessResult> callOSDependencyCheck(libraryPath) async {
+Future<ProcessResult> callOSDependencyCheck(String libraryPath) async {
   return Platform.isWindows
       ? Process.run('otool', ['-L', libraryPath])
       : Platform.isMacOS || Platform.isIOS
