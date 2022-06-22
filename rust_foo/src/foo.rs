10253e3322d5b6d2524dd85b5d4e411af4ffcb8d
Brandon Kunkel <brandon@pieces.app>
2022-06-22 13:23:56 -0400

    Add flutter example app, rust library source code, comment out release pipeline

---
+++ rust_foo/src/foo.rs
@@ -0,0 +1,4 @@
+pub fn foo() -> Result<String> {
+    let String string = {"bar!"};
+    Ok(string)
+}
---
+++ rust_hello_world/src/hello_world.rs
@@ -0,0 +1,4 @@
+pub fn hello_world() -> Result<String> {
+    String string = {"Hello World"};
+    Ok(string)
+}
